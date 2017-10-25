bcedd6f3275306b38342019c9d20fc0abc19448e
Woochan Kim <teakan7179@gmail.com>
2017-10-26 01:01:36 +0900

    Init

---
+++ AR_Messenger/Assets/CustomPipe.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Pipes;
+using System.Threading;
+
+public class CustomPipe : MonoBehaviour
+{
+    NamedPipeClientStream receivingPipe = new NamedPipeClientStream("PcToUnity");
+    //NamedPipeServerStream sendingPipe = new NamedPipeServerStream("ArToPc");
+
+    string receivingStatus;
+    string receivingData;
+
+    string sendingStatus;
+    string sendingData;
+
+    byte[] receivingBuffer = new byte[1024];
+    byte[] sendingBuffer = new byte[1024];
+
+    // Use this for initialization
+    void Start()
+    {
+        receivingPipe.ReadMode = PipeTransmissionMode.Message;
+        SetReceivingStatus("Not connected");
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (receivingPipe.IsConnected)
+        {
+            ReadData();
+        }
+        else
+        {
+            if (IsInvoking("ConnectPipe") == false)
+            {
+                Invoke("ConnectPipe", 1f);
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), receivingStatus);
+        GUI.Label(new Rect(0, 10, Screen.width, Screen.height), receivingData);
+        //GUI.Label(new Rect(10, 0, Screen.width, Screen.height), receivingStatus);
+        //GUI.Label(new Rect(10, -10, Screen.width, Screen.height), receivingData);
+    }
+
+    void OnApplicationQuit()
+    {
+        receivingPipe.Close();
+    }
+
+    private void ConnectPipe()
+    {
+        receivingPipe.Connect(1);
+        if (receivingPipe.IsConnected)
+        {
+            receivingStatus = "Receiving pipe connected!";
+        }
+    }
+
+    /*private void ReadData()
+    {
+        while (receivingPipe.IsConnected)
+        {
+            int bytesRead = receivingPipe.Read(receivingBuffer, 0, 1024);
+            if (bytesRead > 0)
+            {
+                receivingData = System.Text.Encoding.ASCII.GetString(receivingBuffer).Trim();
+            }
+        }
+    }*/
+
+    private void ReadData()
+    {
+        if (receivingPipe.IsConnected)
+        {
+            int bytesRead = receivingPipe.Read(receivingBuffer, 0, 1024);
+            if (bytesRead > 0)
+            {
+                receivingData = System.Text.Encoding.ASCII.GetString(receivingBuffer).Trim();
+                //Debug.Log(receivingData);
+            }
+        }
+    }
+
+    private void StopReceivingPipe()
+    {
+        Debug.Log("HERE!?");
+        receivingPipe.Close();
+        Debug.Log("HERE2!?");
+    }
+
+    private void SetReceivingStatus(string status)
+    {
+        receivingStatus = "Receiving Status : " + status;
+    }
+}
+
---
+++ CustomMessenger/CustomMessenger/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace CustomMessenger
+{
+	public partial class Form1 : Form
+	{
+		NamedPipeClientStream recevingPipe;
+
+		Thread sendingThread;
+		NamedPipeServerStream sendingPipe;
+
+		int dataCount = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			sendingThread = new Thread(ConnectPipe);
+			sendingThread.Start();
+		}
+
+		public void ConnectPipe()
+		{
+			try
+			{
+				sendingPipe = new NamedPipeServerStream("PcToUnity", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous, 1024, 1024);
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show("An error has occurred while pipe open.\n" + e.Message);
+			}
+			IAsyncResult pipeCall = sendingPipe.BeginWaitForConnection(null, null);
+
+			while (!pipeCall.IsCompleted == false)
+			{
+				label1.Text = "Waiting for connection";
+			}
+			sendingPipe.EndWaitForConnection(pipeCall);
+
+			while (sendingPipe.IsConnected)
+			{
+				label1.Text = "Connecting";
+				try
+				{
+					byte[] data;
+					data = Encoding.ASCII.GetBytes(textBox1.Text + "\n" + dataCount);
+					sendingPipe.Write(data, 0, data.Length);
+					sendingPipe.Flush();
+					sendingPipe.WaitForPipeDrain();
+					dataCount += 1;
+				}
+				catch (Exception ex)
+				{
+					if (!ex.Message.StartsWith("Pipe is broken."))
+					{
+						MessageBox.Show("An error has occurred while seding data. \n" + ex.Message);
+					}
+				}
+			}
+			sendingPipe.Close();
+		}
+	}
+}
