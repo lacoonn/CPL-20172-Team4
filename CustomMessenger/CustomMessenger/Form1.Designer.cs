9db2ced6c48cb7caf38adaf12bcf48d91ebbac8d
Woochan Kim <teakan7179@gmail.com>
2017-10-31 23:21:16 +0900

    Upload window messenger

---
+++ CustomMessenger/CustomMessenger/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CustomMessenger
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(0, 330);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(523, 47);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 12);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(107, 416);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Send";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(0, 43);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(523, 269);
+			this.textBox2.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(347, 416);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Close";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(474, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 12);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "label2";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(437, 341);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "전송";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(143, 9);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(228, 23);
+			this.button4.TabIndex = 8;
+			this.button4.Text = "외부 알림 수신";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(524, 474);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
---
+++ CustomMessenger/CustomMessenger/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.Threading;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace CustomMessenger
+{
+	public partial class Form1 : Form
+	{
+		Thread sendingThread;
+		Thread receivingThread;
+
+		NamedPipeClientStream sendingPipe;
+		NamedPipeServerStream receivingPipe;
+
+		string sendingPipeName;
+		string receivingPipeName;
+
+		PacketData packetData;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			sendingPipeName = "wtou";
+			receivingPipeName = "utow";
+
+			packetData = new PacketData();
+			packetData.messageLog = new List<PacketData.Message>();
+
+			try
+			{
+				sendingPipe = new NamedPipeClientStream(sendingPipeName);
+				receivingPipe = new NamedPipeServerStream(receivingPipeName);
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show("An error has occurred while pipe open.\n" + e.Message);
+				return;
+			}
+
+			button1.Click += new EventHandler(this.Button1Click);
+			button2.Click += new EventHandler(this.Button2Click);
+			button3.Click += new EventHandler(this.Button3Click);
+			button4.Click += new EventHandler(this.Button4Click);
+
+			sendingThread = new Thread(ConnectSendingPipe);
+			sendingThread.Start();
+			receivingThread = new Thread(ConnectReceivingPipe);
+			receivingThread.Start();
+		}
+
+		public void ConnectSendingPipe()
+		{
+			label1.Text = "Not connected";
+			while (!sendingPipe.IsConnected)
+			{
+				label1.Text = "Waiting";
+				sendingPipe.Connect();
+			}
+		}
+
+		public void ConnectReceivingPipe()
+		{
+			label2.Text = "Not connected";
+			receivingPipe.WaitForConnection();
+
+			while (receivingPipe.IsConnected)
+			{
+				label2.Text = "Wait data";
+				StreamReader reader = new StreamReader(receivingPipe);
+				string xmlData;
+				xmlData = reader.ReadLine();
+				if (xmlData != null)
+				{
+					label2.Text = "Receive data";
+					packetData = DeserializeFromXML(xmlData);
+
+					textBox2.Text = "";
+					foreach (PacketData.Message tempMessage in packetData.messageLog)
+					{
+						if (tempMessage.isMe)
+							textBox2.Text += tempMessage.text;
+						else
+							textBox2.Text += "\t" + tempMessage.text;
+						textBox2.Text += "\r\n";
+					}
+				}
+			}
+		}
+
+		public static string SerializeToXml(PacketData data)
+		{
+			string xmlData = null;
+
+			StringWriter stringWriter = null;
+
+			XmlSerializer serializer = new XmlSerializer(data.GetType());
+			stringWriter = new StringWriter();
+			serializer.Serialize(stringWriter, data);
+
+			xmlData = stringWriter.ToString().Replace(Environment.NewLine, " ");
+
+			stringWriter.Close();
+
+			return xmlData;
+		}
+
+		public static PacketData DeserializeFromXML(string xmlData)
+		{
+			PacketData data = null;
+
+			StringReader stringReader = null;
+
+			XmlSerializer deserializer = new XmlSerializer(typeof(PacketData));
+			stringReader = new StringReader(xmlData);
+			data = (PacketData)deserializer.Deserialize(stringReader);
+
+			return data;
+		}
+
+		// send button click
+		public void Button1Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		public void Button2Click(object sender, EventArgs e)
+		{
+			if (sendingPipe.IsConnected)
+				sendingPipe.Close();
+			if (sendingThread.IsAlive)
+				sendingThread.Abort();
+
+			if (receivingPipe.IsConnected)
+				receivingPipe.Close();
+			if (receivingThread.IsAlive)
+				receivingThread.Abort();
+			label2.Text = "Closed";
+		}
+
+		public void Button3Click(object sender, EventArgs e)
+		{
+			// 전송 버튼
+			PacketData.Message temp;
+			temp.text = textBox1.Text;
+			temp.isMe = true;
+			packetData.messageLog.Add(temp);
+			textBox1.Text = "";
+
+			if (sendingPipe.IsConnected)
+			{
+				try
+				{
+					// 로그 텍스트박스 업데이트
+					textBox2.Text = "";
+					foreach (PacketData.Message tempMessage in packetData.messageLog)
+					{
+						if (tempMessage.isMe)
+							textBox2.Text += tempMessage.text;
+						else
+							textBox2.Text += "\t" + tempMessage.text;
+						textBox2.Text += "\r\n";
+					}
+					// 데이터 전송
+					string xmlData = SerializeToXml(packetData);
+					StreamWriter writer = new StreamWriter(sendingPipe);
+					writer.WriteLine(xmlData);
+					writer.Flush();
+				}
+				catch (Exception ex)
+				{
+					if (!ex.Message.StartsWith("Pipe is broken."))
+					{
+						MessageBox.Show("An error has occurred while seding data. \n" + ex.Message);
+						return;
+					}
+				}
+			}
+		}
+
+		public void Button4Click(object sender, EventArgs e)
+		{
+			// 외부 알림 수신 버튼
+			PacketData.Message temp;
+			temp.text = "This is external message";
+			temp.isMe = false;
+			packetData.messageLog.Add(temp);
+
+			if (sendingPipe.IsConnected)
+			{
+				try
+				{
+					// 로그 텍스트박스 업데이트
+					textBox2.Text = "";
+					foreach (PacketData.Message tempMessage in packetData.messageLog)
+					{
+						if (tempMessage.isMe)
+							textBox2.Text += tempMessage.text;
+						else
+							textBox2.Text += "\t" + tempMessage.text;
+						textBox2.Text += "\r\n";
+					}
+					// 데이터 전송
+					string xmlData = SerializeToXml(packetData);
+					StreamWriter writer = new StreamWriter(sendingPipe);
+					writer.WriteLine(xmlData);
+					writer.Flush();
+				}
+				catch (Exception ex)
+				{
+					if (!ex.Message.StartsWith("Pipe is broken."))
+					{
+						MessageBox.Show("An error has occurred while seding data. \n" + ex.Message);
+						return;
+					}
+				}
+			}
+		}
+
+		private void Form1_Closed(object sender, System.EventArgs e)
+		{
+			if (sendingPipe.IsConnected)
+				sendingPipe.Close();
+			if (sendingThread.IsAlive)
+				sendingThread.Abort();
+
+			if (receivingPipe.IsConnected)
+				receivingPipe.Close();
+			if (receivingThread.IsAlive)
+				receivingThread.Abort();
+		}
+	}
+}
