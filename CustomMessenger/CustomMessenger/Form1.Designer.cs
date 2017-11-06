e73a69ba638694c4ef6ce7329d4584778b292505
Woochan Kim <teakan7179@gmail.com>
2017-11-06 20:15:17 +0900

    Add bidirectional communication between 2 messenger program without server

--- CustomMessenger/CustomMessenger/Form1.Designer.cs
+++ CustomMessenger/CustomMessenger/Form1.Designer.cs
@@ -30,20 +30,28 @@
 		{
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
+			this.Connect = new System.Windows.Forms.Button();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.button2 = new System.Windows.Forms.Button();
 			this.label2 = new System.Windows.Forms.Label();
 			this.button3 = new System.Windows.Forms.Button();
 			this.button4 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(0, 330);
+			this.textBox1.Location = new System.Drawing.Point(12, 433);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(523, 47);
+			this.textBox1.Size = new System.Drawing.Size(501, 47);
 			this.textBox1.TabIndex = 0;
 			// 
 			// label1
@@ -55,26 +63,26 @@
 			this.label1.TabIndex = 1;
 			this.label1.Text = "label1";
 			// 
-			// button1
+			// Connect
 			// 
-			this.button1.Location = new System.Drawing.Point(107, 416);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "Send";
-			this.button1.UseVisualStyleBackColor = true;
+			this.Connect.Location = new System.Drawing.Point(421, 79);
+			this.Connect.Name = "Connect";
+			this.Connect.Size = new System.Drawing.Size(75, 23);
+			this.Connect.TabIndex = 2;
+			this.Connect.Text = "Connect";
+			this.Connect.UseVisualStyleBackColor = true;
 			// 
 			// textBox2
 			// 
-			this.textBox2.Location = new System.Drawing.Point(0, 43);
+			this.textBox2.Location = new System.Drawing.Point(12, 155);
 			this.textBox2.Multiline = true;
 			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(523, 269);
+			this.textBox2.Size = new System.Drawing.Size(501, 260);
 			this.textBox2.TabIndex = 3;
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(347, 416);
+			this.button2.Location = new System.Drawing.Point(217, 521);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
 			this.button2.TabIndex = 4;
@@ -84,7 +92,7 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(474, 9);
+			this.label2.Location = new System.Drawing.Point(12, 28);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(38, 12);
 			this.label2.TabIndex = 5;
@@ -92,7 +100,7 @@
 			// 
 			// button3
 			// 
-			this.button3.Location = new System.Drawing.Point(437, 341);
+			this.button3.Location = new System.Drawing.Point(421, 445);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(75, 23);
 			this.button3.TabIndex = 7;
@@ -101,24 +109,103 @@
 			// 
 			// button4
 			// 
-			this.button4.Location = new System.Drawing.Point(143, 9);
+			this.button4.Location = new System.Drawing.Point(153, 9);
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(228, 23);
 			this.button4.TabIndex = 8;
 			this.button4.Text = "외부 알림 수신";
 			this.button4.UseVisualStyleBackColor = true;
 			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(387, 9);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(38, 12);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "label3";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(387, 28);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(38, 12);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "label4";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(281, 61);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 21);
+			this.textBox3.TabIndex = 11;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(77, 100);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 21);
+			this.textBox4.TabIndex = 12;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(281, 100);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 21);
+			this.textBox5.TabIndex = 13;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(230, 64);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(45, 12);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "내 포트";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(27, 103);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(44, 12);
+			this.label6.TabIndex = 15;
+			this.label6.Text = "연결 IP";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(218, 103);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(57, 12);
+			this.label7.TabIndex = 16;
+			this.label7.Text = "연결 포트";
+			this.label7.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(524, 474);
+			this.ClientSize = new System.Drawing.Size(524, 568);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.button4);
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.Connect);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
 			this.Name = "Form1";
@@ -132,12 +219,20 @@
 
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button Connect;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
 	}
 }
 
--- CustomMessenger/CustomMessenger/Form1.cs
+++ CustomMessenger/CustomMessenger/Form1.cs
@@ -10,6 +10,8 @@ using System.IO.Pipes;
 using System.Threading;
 using System.Xml.Serialization;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace CustomMessenger
 {
@@ -17,14 +19,23 @@ namespace CustomMessenger
 	{
 		Thread sendingThread;
 		Thread receivingThread;
+		Thread messengerClientThread;
+		Thread messengerServerThread;
 
 		NamedPipeClientStream sendingPipe;
 		NamedPipeServerStream receivingPipe;
+
+		Socket client;
 
 		string sendingPipeName;
 		string receivingPipeName;
 
 		PacketData packetData;
+
+		bool isSendingPipeConnected;
+		bool isReceivingPipeConnected;
+		bool isMessengerClientConnected;
+		bool isMessengerServerConnected;
 
 		public Form1()
 		{
@@ -38,8 +49,8 @@ namespace CustomMessenger
 
 			try
 			{
-				sendingPipe = new NamedPipeClientStream(sendingPipeName);
-				receivingPipe = new NamedPipeServerStream(receivingPipeName);
+				//sendingPipe = new NamedPipeClientStream(sendingPipeName);
+				//receivingPipe = new NamedPipeServerStream(receivingPipeName);
 			}
 			catch (Exception e)
 			{
@@ -47,15 +58,20 @@ namespace CustomMessenger
 				return;
 			}
 
-			button1.Click += new EventHandler(this.Button1Click);
+			Connect.Click += new EventHandler(this.Button1Click);
 			button2.Click += new EventHandler(this.Button2Click);
 			button3.Click += new EventHandler(this.Button3Click);
 			button4.Click += new EventHandler(this.Button4Click);
 
-			sendingThread = new Thread(ConnectSendingPipe);
+			/*sendingThread = new Thread(ConnectSendingPipe);
 			sendingThread.Start();
 			receivingThread = new Thread(ConnectReceivingPipe);
-			receivingThread.Start();
+			receivingThread.Start();*/
+
+			/*messengerClientThread = new Thread(MessengerClient);
+			messengerClientThread.Start();
+			messengerServerThread = new Thread(MessengerServer);
+			messengerServerThread.Start();*/
 		}
 
 		public void ConnectSendingPipe()
@@ -127,10 +143,111 @@ namespace CustomMessenger
 			return data;
 		}
 
-		// send button click
+		public void MessengerServer()
+		{
+			string myPort = textBox3.Text; // 내 포트 번호
+			IPEndPoint ipep = new IPEndPoint(IPAddress.Any, Int32.Parse(myPort));
+			Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			server.Bind(ipep);
+			label3.Text = "Server:Listen";
+			server.Listen(20);
+			label3.Text = "Server:Listening";
+
+			Socket connectedClient = server.Accept();
+			label3.Text = "Server:Accept";
+			isMessengerServerConnected = true;
+			while (isMessengerServerConnected)
+			{
+				IPEndPoint ip = (IPEndPoint)connectedClient.RemoteEndPoint;
+				// "주소 {0}에서 접속", ip.Address
+
+				String _buf = "명월 서버에 오신 걸 환영합니다.";
+				Byte[] _data = Encoding.Default.GetBytes(_buf);
+				//client.Send(_data);
+				_data = new Byte[1024];
+				connectedClient.Receive(_data);
+				_buf = Encoding.Default.GetString(_data);
+				AddMessageLog(_buf, false);
+				// _buf 출력
+				label3.Text = "Server:Receve message";
+			}
+			label3.Text = "Server:End";
+			connectedClient.Close();
+			server.Close();
+		}
+
+		public void MessengerClient()
+		{
+			string connectIP = textBox4.Text;
+			string connectPort = textBox5.Text;
+			IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(connectIP), Int32.Parse(connectPort));
+			client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			int count = 1;
+			while (isMessengerClientConnected == false)
+			{
+				try
+				{
+					Thread.Sleep(1000);
+					label4.Text = "Client:trying " + count.ToString();
+					count++;
+					//label4.Text = IPAddress.Parse(connectIP).ToString();
+					//label4.Text = "HERE:" + Int32.Parse(connectPort).ToString();
+					client.Connect(ipep);
+					isMessengerClientConnected = true;
+				}
+				catch
+				{
+					isMessengerClientConnected = false;
+				}
+			}
+			label4.Text = "Client:connect";
+
+			/*Byte[] _data = new Byte[1024];
+			//client.Receive(_data);
+			String _buf = Encoding.Default.GetString(_data);
+			// Console.WriteLine(_buf);
+			_buf = "소켓 접속 확인 됐습니다.";
+			_data = Encoding.Default.GetBytes(_buf);
+			client.Send(_data);*/
+
+			while (isMessengerClientConnected)
+			{
+				Thread.Sleep(1000);
+			}
+
+			client.Close();
+
+			// 종료 확인
+		}
+
+		public void AddMessageLog(string message, bool isInternal)
+		{
+			// 메세지 로그 생성
+			PacketData.Message temp;
+			temp.text = message;
+			temp.isMe = isInternal;
+			packetData.messageLog.Add(temp);
+
+			// 로그 텍스트박스 업데이트
+			textBox2.Text = "";
+			foreach (PacketData.Message tempMessage in packetData.messageLog)
+			{
+				if (tempMessage.isMe)
+					textBox2.Text += tempMessage.text;
+				else
+					textBox2.Text += "\t" + tempMessage.text;
+				textBox2.Text += "\r\n";
+			}
+		}
+
+		// 연결(Connect) 버튼
 		public void Button1Click(object sender, EventArgs e)
 		{
-			
+			messengerClientThread = new Thread(MessengerClient);
+			messengerClientThread.Start();
+			messengerServerThread = new Thread(MessengerServer);
+			messengerServerThread.Start();
 		}
 
 		public void Button2Click(object sender, EventArgs e)
@@ -144,52 +261,60 @@ namespace CustomMessenger
 				receivingPipe.Close();
 			if (receivingThread.IsAlive)
 				receivingThread.Abort();
+
+			isMessengerClientConnected = false;
+			isMessengerServerConnected = false;
+
 			label2.Text = "Closed";
 		}
 
+		// 전송 버튼
 		public void Button3Click(object sender, EventArgs e)
 		{
-			// 전송 버튼
-			PacketData.Message temp;
-			temp.text = textBox1.Text;
-			temp.isMe = true;
-			packetData.messageLog.Add(temp);
+			// 메세지 로그 업데이트
+			string currentMessage = textBox1.Text;
 			textBox1.Text = "";
+			AddMessageLog(currentMessage, true);
 
-			if (sendingPipe.IsConnected)
+			// 상대방에게 데이터 전송
+			if (isMessengerClientConnected && isMessengerServerConnected)
 			{
-				try
+				Byte[] _data = new Byte[1024];
+				String _buf;
+				_buf = currentMessage;
+				_data = Encoding.Default.GetBytes(_buf);
+				client.Send(_data);
+			}
+
+			// AR로 데이터 전송
+			//if (sendingPipe.IsConnected && receivingPipe.IsConnected)
+			if (isSendingPipeConnected && isReceivingPipeConnected)
+			{
+				if (sendingPipe.IsConnected)
 				{
-					// 로그 텍스트박스 업데이트
-					textBox2.Text = "";
-					foreach (PacketData.Message tempMessage in packetData.messageLog)
+					try
 					{
-						if (tempMessage.isMe)
-							textBox2.Text += tempMessage.text;
-						else
-							textBox2.Text += "\t" + tempMessage.text;
-						textBox2.Text += "\r\n";
+						// 데이터 전송
+						string xmlData = SerializeToXml(packetData);
+						StreamWriter writer = new StreamWriter(sendingPipe);
+						writer.WriteLine(xmlData);
+						writer.Flush();
 					}
-					// 데이터 전송
-					string xmlData = SerializeToXml(packetData);
-					StreamWriter writer = new StreamWriter(sendingPipe);
-					writer.WriteLine(xmlData);
-					writer.Flush();
-				}
-				catch (Exception ex)
-				{
-					if (!ex.Message.StartsWith("Pipe is broken."))
+					catch (Exception ex)
 					{
-						MessageBox.Show("An error has occurred while seding data. \n" + ex.Message);
-						return;
+						if (!ex.Message.StartsWith("Pipe is broken."))
+						{
+							MessageBox.Show("An error has occurred while seding data. \n" + ex.Message);
+							return;
+						}
 					}
 				}
 			}
 		}
 
+		// 외부 알림 수신 버튼
 		public void Button4Click(object sender, EventArgs e)
 		{
-			// 외부 알림 수신 버튼
 			PacketData.Message temp;
 			temp.text = "This is external message";
 			temp.isMe = false;
@@ -237,6 +362,9 @@ namespace CustomMessenger
 				receivingPipe.Close();
 			if (receivingThread.IsAlive)
 				receivingThread.Abort();
+
+			isMessengerClientConnected = false;
+			isMessengerServerConnected = false;
 		}
 	}
 }
