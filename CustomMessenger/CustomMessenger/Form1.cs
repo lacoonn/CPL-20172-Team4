229cac0138fe1dc19a5eaa333c237922fde2b890
Woochan Kim <teakan7179@gmail.com>
2017-11-28 21:18:18 +0900

    Add comments

--- CustomMessenger/CustomMessenger/Form1.cs
+++ CustomMessenger/CustomMessenger/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Threading;
-using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
 using System.Net;
@@ -23,41 +22,50 @@ namespace CustomMessenger
 {
 	public partial class Form1 : Form
 	{
+		// AR 인터프로세스 통신 스레드
 		Thread sendingThread;
 		Thread receivingThread;
+		// 메신저 스레드
 		Thread messengerClientThread;
 		Thread messengerServerThread;
+		// 구글 캘린더 스레드
 		Thread googleCalendarThread;
 
+		// AR 프로세스 통신 소켓
 		Socket serverAR;
 		Socket connectedClientAR;
 		Socket clientAR;
 		bool isArClientConnected;
 		bool isArServerConnected;
 
+		// 메신저 통신 소켓
 		Socket server;
 		Socket connectedClient;
 		Socket client;
 		bool isMessengerClientConnected;
 		bool isMessengerServerConnected;
 
+		// AR 통신 포트 번호
 		string winPort;
 		string arPort;
 
+		// 전송 데이터
 		PacketData packetData;
 
 		string myName = "석문주";
-		string yourName = "기몽재";
+		string yourName = "김홍재";
 
 		// Google Calendar
 		static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
 		static string ApplicationName = "Google Calendar API .NET Quickstart";
 		static List<string> eventIdList = new List<string>();
 
+		// 폼 초기화
 		public Form1()
 		{
 			InitializeComponent();
 
+			// AR 통신 포트 설정
 			winPort = "8282";
 			arPort = "8283";
 
@@ -70,19 +78,24 @@ namespace CustomMessenger
 			button4.Click += new EventHandler(this.Button4Click);
 			FormClosing += Form1_Closing;
 
+			// 구글 캘린더 스레드 실행
 			googleCalendarThread = new Thread(GoogleCalendar);
 			googleCalendarThread.Start();
 		}
 
+		// 구글 캘린더 스레드 함수
 		public void GoogleCalendar()
 		{
 			while (!isArClientConnected || !isArServerConnected)
 			{
 				Thread.Sleep(1000); // 캘린더 스레드는 AR 프로그램의 연결을 기다린다.
 			}
+
 			int loopCount = 0;
+
 			while (true)
 			{
+				// 구글 캘린더 계정 인증
 				UserCredential credential;
 
 				using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
@@ -100,14 +113,14 @@ namespace CustomMessenger
 					//WriteTextBox2("Credential file saved to: " + credPath);
 				}
 
-				// Create Google Calendar API service.
+				// 구글 캘린더 API 서비스 생성
 				var service = new CalendarService(new BaseClientService.Initializer()
 				{
 					HttpClientInitializer = credential,
 					ApplicationName = ApplicationName,
 				});
 
-				// Define parameters of request.
+				// 리퀘스트 생성
 				EventsResource.ListRequest request = service.Events.List("primary");
 				request.TimeMin = DateTime.Now;
 				request.ShowDeleted = false;
@@ -115,31 +128,30 @@ namespace CustomMessenger
 				request.MaxResults = 10;
 				request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
 
-				// List events.
+				// 이벤트 수신
 				Events events = request.Execute();
 				//WriteTextBox2("Upcoming events >>");
-				if (events.Items != null && events.Items.Count > 0)
+				if (events.Items != null && events.Items.Count > 0) // 이벤트(일정)이 있을 경우
 				{
 					foreach (var eventItem in events.Items)
 					{
 						string when = eventItem.Start.DateTime.ToString();
-						if (String.IsNullOrEmpty(when)) // 종일로 설정되어 있을 경우
+						if (String.IsNullOrEmpty(when)) // 일정 시간이 종일로 설정되어 있을 경우
 						{
 							when = eventItem.Start.Date;
 							DateTime dateTime;
 
+							// 일정 시간 파싱
 							if (DateTime.TryParse(when, out dateTime))
 							{
 								TimeSpan timeDiff = dateTime - DateTime.Now;
-								// 이벤트 시간 파싱을 성공했을 경우(여기에 필요한 행위가 들어감)
 								//WriteTextBox2(eventItem.Id + " _ " + eventItem.Summary + " // Min diff: " + timeDiff.TotalMinutes);
 
-								// eventItem.OriginalStartTime = 반복 이벤트
-
+								// 남은 시간이 1시간 이내일 경우
 								if (0 <= timeDiff.TotalMinutes && timeDiff.TotalMinutes <= 60)
 								{
 									bool isItemExist = false;
-									foreach (string eventIdItem in eventIdList)
+									foreach (string eventIdItem in eventIdList) // 이미 전송한 알림인지 확인
 									{
 										if(eventItem.Id.Equals(eventIdItem))
 										{
@@ -147,15 +159,14 @@ namespace CustomMessenger
 											break;
 										}
 									}
-									if (isItemExist)
+									if (isItemExist) // 이미 전송한 알림이라면
 									{
 										// Pass
 									}
-									else
+									else // 아직 전송하지 않은 알림이라면
 									{
-										eventIdList.Add(eventItem.Id);
+										eventIdList.Add(eventItem.Id); // 전송한 알림으로 등록하고
 										SendCalendarAlarmToAr(eventItem.Summary, when); // 캘린더 알림 AR로 전송
-										//MessageBox.Show(eventItem.Summary + when);
 									}
 								}
 							}
@@ -164,19 +175,17 @@ namespace CustomMessenger
 								WriteTextBox2("Date Error");
 							}
 						}
-						else // 정확한 시간이 설정되어 있을 경우
+						else // 일정이 정확한 시간이 설정되어 있을 경우
 						{
 							DateTime dateTime = eventItem.Start.DateTime.Value;
 							TimeSpan timeDiff = dateTime - DateTime.Now;
-							// 이벤트 시간 파싱을 성공했을 경우(여기에 필요한 행위가 들어감)
 							//WriteTextBox2(eventItem.Id + " _ " + eventItem.Summary + " // Min diff: " + timeDiff.TotalMinutes);
 
-							// eventItem.OriginalStartTime = 반복 이벤트
-
+							// 남은 시간이 1시간 이내일 경우
 							if (0 <= timeDiff.TotalMinutes && timeDiff.TotalMinutes <= 60)
 							{
 								bool isItemExist = false;
-								foreach (string eventIdItem in eventIdList)
+								foreach (string eventIdItem in eventIdList) // 이미 전송한 알림인지 확인
 								{
 									if (eventItem.Id.Equals(eventIdItem))
 									{
@@ -184,21 +193,20 @@ namespace CustomMessenger
 										break;
 									}
 								}
-								if (isItemExist)
+								if (isItemExist) // 이미 전송한 알림이라면
 								{
 									// Pass
 								}
-								else
+								else // 아직 전송하지 않은 알림이라면
 								{
-									eventIdList.Add(eventItem.Id);
+									eventIdList.Add(eventItem.Id); // 전송한 알림으로 등록하고
 									SendCalendarAlarmToAr(eventItem.Summary, when); // 캘린더 알림 AR로 전송
-									//MessageBox.Show(eventItem.Summary + when);
 								}
 							}
 						}
 					}
 				}
-				else
+				else // 일정이 없을 경우
 				{
 					//MessageBox.Show("No upcoming events found.");
 				}
@@ -238,7 +246,6 @@ namespace CustomMessenger
 		// 캘린더에서 임박한 알림을 Ar로 전송
 		public void SendCalendarAlarmToAr(string eventSummary, string when)
 		{
-			//MessageBox.Show(eventSummary);
 			try
 			{
 				// PacketData에 캘린더 알림 추가
@@ -266,6 +273,7 @@ namespace CustomMessenger
 			}
 		}
 
+		// 메신저간 통신에 사용되는 스레드 서버(수신) 함수
 		public void MessengerServer()
 		{
 			string myPort = textBox3.Text; // 내 포트 번호
@@ -277,20 +285,23 @@ namespace CustomMessenger
 			server.Listen(1);
 			label3.Text = "Server:Listening";
 
+			// 상대 메신저의 클라이언트가 접속
 			connectedClient = server.Accept();
 			label3.Text = "Server:Accept";
 			isMessengerServerConnected = true;
+			// 상대 메신저의 클라이언트가 접속해 있는 동안
 			while (isMessengerServerConnected)
 			{
-				IPEndPoint ip = (IPEndPoint)connectedClient.RemoteEndPoint;
+				// 상대방 IP
+				//IPEndPoint ip = (IPEndPoint)connectedClient.RemoteEndPoint;
 				//WriteTextBox2("Server:" + ip.Address);
 
+				// 스트림 생성
 				string _buf = "";
-				Byte[] _data = new Byte[1024];
 				NetworkStream network = new NetworkStream(connectedClient);
 				StreamReader reader = new StreamReader(network);
-				//_buf = reader.ReadLine();
 
+				// 스트림에서 모든 라인을 수신
 				while (true)
 				{
 					label3.Text = "Server:Reading";
@@ -302,6 +313,7 @@ namespace CustomMessenger
 						break;
 				}
 
+				// 수신한 메세지를 로그에 추가하고 화면 업데이트
 				AddMessageLog(_buf, false);
 				UpdateTextBox2();
 
@@ -314,7 +326,7 @@ namespace CustomMessenger
 					{
 						try
 						{
-							// 데이터 전송
+							// 데이터를 XML화해서 스트림으로 데이터 전송
 							NetworkStream networkStream = new NetworkStream(clientAR);
 							string xmlData = SerializeToXml(packetData);
 							StreamWriter writer = new StreamWriter(networkStream);
@@ -332,14 +344,17 @@ namespace CustomMessenger
 					}
 				}
 			}
+			// 종료
 			label3.Text = "Server:End";
 			connectedClient.Close();
 			server.Close();
 			isMessengerServerConnected = false;
 		}
 
+		// 메신저간 통신에 사용되는 클라이언트(통신) 스레드 함수
 		public void MessengerClient()
 		{
+			// 상대 메신저 서버에 연결
 			string connectIP = textBox4.Text;
 			//string connectIP = "127.0.0.1";
 			string connectPort = textBox5.Text;
@@ -353,8 +368,6 @@ namespace CustomMessenger
 					Thread.Sleep(1000);
 					label4.Text = "Client:trying " + count.ToString();
 					count++;
-					//label4.Text = IPAddress.Parse(connectIP).ToString();
-					//label4.Text = "HERE:" + Int32.Parse(connectPort).ToString();
 					client.Connect(ipep);
 					isMessengerClientConnected = true;
 				}
@@ -365,18 +378,22 @@ namespace CustomMessenger
 			}
 			label4.Text = "Client:connect";
 
+			// 연결 성공 시 대기
 			while (isMessengerClientConnected)
 			{
 				Thread.Sleep(100);
 			}
 
+			// 종료
 			label4.Text = "Client:End";
 			client.Close();
 			isMessengerClientConnected = false;
 		}
 
+		// AR 프로세스와 통신에 사용되는 서버(수신) 스레드 함수
 		public void ConnectReceivingPipe()
 		{
+			// AR 프로그램 클라이언트 연결 대기
 			string myPort = winPort; // 내 포트 번호
 			IPEndPoint ipep = new IPEndPoint(IPAddress.Any, Int32.Parse(myPort));
 			serverAR = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -386,14 +403,13 @@ namespace CustomMessenger
 			serverAR.Listen(1);
 			label1.Text = "Server:Listening";
 
+			// AR 프로그램 클라이언트 연결
 			connectedClientAR = serverAR.Accept();
 			label1.Text = "Server:Accept";
 			isArServerConnected = true;
 			while (isArServerConnected)
 			{
-				IPEndPoint ip = (IPEndPoint)connectedClientAR.RemoteEndPoint;
-				//WriteTextBox2("Server:" + ip.Address);
-
+				// 스트림을 생성하고 데이터 수신을 대기
 				label1.Text = "Wait data";
 				NetworkStream networkStream = new NetworkStream(connectedClientAR);
 				StreamReader reader = new StreamReader(networkStream);
@@ -408,39 +424,36 @@ namespace CustomMessenger
 				}
 				if (!xmlData.Equals(""))
 				{
+					// 수신된 xml 데이터를 파싱해서 통신 데이터와 화면을 업데이트
 					label1.Text = "Receive data";
 					packetData = DeserializeFromXML(xmlData);
-
 					UpdateTextBox2();
 
 					// 상대방에게 데이터 전송
 					if (isMessengerClientConnected && isMessengerServerConnected)
 					{
-						//Byte[] _data = new Byte[1024];
-						//String _buf;
-
-						//_buf = currentMessage + '\n';
-						//_data = Encoding.Default.GetBytes(_buf);
-						//client.Send(_data);
-
+						// 스트림을 생성
 						NetworkStream network = new NetworkStream(client);
 						StreamWriter writer = new StreamWriter(network);
+						// 마지막 메세지를 전송
 						string currentMessage = packetData.messageLog[packetData.messageLog.Count - 1].text;
-
-						//writer.WriteLine(currentMessage);
 						writer.WriteLine(currentMessage.ToCharArray(), 0, currentMessage.Length);
 						writer.Flush();
 					}
 				}
 			}
+
+			// 종료
 			label1.Text = "Server:End";
 			connectedClientAR.Close();
 			serverAR.Close();
 			isArServerConnected = false;
 		}
 
+		// AR 프로세스와 통신에 사용되는 클라이언트(통신) 스레드 함수
 		public void ConnectSendingPipe()
 		{
+			// AR 프로세스의 서버에 연결 시도
 			string connectIP = "127.0.0.1";
 			string connectPort = arPort;
 			IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(connectIP), Int32.Parse(connectPort));
@@ -465,16 +478,19 @@ namespace CustomMessenger
 			}
 			label2.Text = "Client:connect";
 
+			// 연결에 성공하면 대기
 			while (isArClientConnected)
 			{
 				Thread.Sleep(100);
 			}
 
+			// 종료
 			label2.Text = "Client:End";
 			clientAR.Close();
 			isArClientConnected = false;
 		}
 
+		//PacketData 클래스를 xml화하는 함수
 		public static string SerializeToXml(PacketData data)
 		{
 			string xmlData = null;
@@ -492,6 +508,7 @@ namespace CustomMessenger
 			return xmlData;
 		}
 
+		//xml 데이터를 PacketData화하는 함수
 		public static PacketData DeserializeFromXML(string xmlData)
 		{
 			PacketData data = null;
@@ -505,21 +522,18 @@ namespace CustomMessenger
 			return data;
 		}
 
-
 		// PacketData의 메세지 로그에 메세지 추가
 		public void AddMessageLog(string message, bool isInternal)
 		{
-			// 메세지 로그 생성
 			PacketData.Message temp;
 			temp.text = message;
 			temp.isMe = isInternal;
 			packetData.messageLog.Add(temp);
 		}
 
-		// 텍스트박스2(메세지 로그 본문)을 현재 메세지 로그로 업데이트
+		// 텍스트박스2(메세지 로그 본문)을 현재 메세지 로그로 업데이트하는 함수
 		public void UpdateTextBox2()
 		{
-			// 로그 텍스트박스 업데이트
 			textBox2.Text = "";
 			foreach (PacketData.Message tempMessage in packetData.messageLog)
 			{
@@ -530,7 +544,7 @@ namespace CustomMessenger
 			}
 		}
 
-		// 텍스트박스2(메세지 로그)에 string 추가
+		// 텍스트박스2(메세지 로그)에 string 추가하는 함수
 		public void WriteTextBox2(string message)
 		{
 			textBox2.Text += message;
@@ -540,6 +554,7 @@ namespace CustomMessenger
 		// 연결(Connect) 버튼
 		public void Button1Click(object sender, EventArgs e)
 		{
+			// 메신저 스레드들을 생성
 			messengerClientThread = new Thread(MessengerClient);
 			messengerClientThread.Start();
 			messengerServerThread = new Thread(MessengerServer);
@@ -549,6 +564,7 @@ namespace CustomMessenger
 		// 종료 버튼
 		public void Button2Click(object sender, EventArgs e)
 		{
+			// 연결된 모든 소켓, 스레드들을 종료
 			try
 			{
 				if (isArClientConnected)
@@ -595,7 +611,7 @@ namespace CustomMessenger
 		// 전송 버튼
 		public void Button3Click(object sender, EventArgs e)
 		{
-			// 메세지 로그 업데이트
+			// 현재 메세지를 로그에 추가하고 화면을 업데이트
 			string currentMessage = textBox1.Text;
 			textBox1.Text = "";
 			AddMessageLog(currentMessage, true);
@@ -604,18 +620,8 @@ namespace CustomMessenger
 			// 상대방에게 데이터 전송
 			if (isMessengerClientConnected && isMessengerServerConnected)
 			{
-				//Byte[] _data = new Byte[1024];
-				//String _buf;
-
-				//_buf = currentMessage + '\n';
-				//_data = Encoding.Default.GetBytes(_buf);
-				//client.Send(_data);
-
 				NetworkStream networkStream = new NetworkStream(client);
 				StreamWriter writer = new StreamWriter(networkStream);
-				//currentMessage += '\n';
-				
-				//writer.WriteLine(currentMessage);
 				writer.WriteLine(currentMessage.ToCharArray(), 0, currentMessage.Length);
 				writer.Flush();
 			}
@@ -627,7 +633,6 @@ namespace CustomMessenger
 				{
 					try
 					{
-						// 데이터 전송
 						NetworkStream networkStream = new NetworkStream(clientAR);
 						string xmlData = SerializeToXml(packetData);
 						StreamWriter writer = new StreamWriter(networkStream);
@@ -646,7 +651,7 @@ namespace CustomMessenger
 			}
 		}
 
-		// 외부 알림 수신 버튼
+		// 외부 알림 수신 버튼(이제 사용하지 않음)
 		public void Button4Click(object sender, EventArgs e)
 		{
 			PacketData.Message temp;
@@ -658,7 +663,6 @@ namespace CustomMessenger
 			{
 				try
 				{
-					// 로그 텍스트박스 업데이트
 					textBox2.Text = "";
 					foreach (PacketData.Message tempMessage in packetData.messageLog)
 					{
@@ -686,6 +690,7 @@ namespace CustomMessenger
 			}
 		}
 
+		// 프로그램 종료 시
 		private void Form1_Closing(object sender, FormClosingEventArgs e)
 		{
 			if (e.CloseReason == CloseReason.WindowsShutDown)
@@ -741,6 +746,7 @@ namespace CustomMessenger
 		// AR 연결 버튼
 		private void button1_Click(object sender, EventArgs e)
 		{
+			// AR 통신 스레드 생성
 			sendingThread = new Thread(ConnectSendingPipe);
 			sendingThread.Start();
 			receivingThread = new Thread(ConnectReceivingPipe);
